d71ec95223aadba4c667ebc3a05441b29194224c
NcabreraM <natalia.cabrera.m@mail.pucv.cl>
2025-03-12 14:00:15 +0000

    Initial commit - Pasqui library

--- src/pasqui/__init__.py
+++ src/pasqui/__init__.py
@@ -1,4 +1,5 @@
 from .module1 import pasqui_converting
 from .module2 import pasqui_embedding
 from .module3 import pasqui_summarising
+from .module3 import pasqui_asks
 from .module4 import pasqui_structuring
