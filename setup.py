839623df5b8e883c78571dfc0824e3115d6b0451
Ncabrera-Morales <natalia.cabrera.m@mail.pucv.cl>
2025-03-16 21:44:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/NcabreraM/pasqui',
-    license='MIT'
+    license='MIT',
 )
